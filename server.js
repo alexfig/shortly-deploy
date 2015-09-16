a16fde201eea875b97614dbb171581634fa491eb
Hack Reactor Students <accounts@hackreactor.com>
2015-09-16 12:03:35 -0700

    Added environment variables

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var app = require('./server-config.js');
 
-var port = 4568;
+var port = process.env.PORT || 4568;
 
+//var ip = process.env.IP || '127.0.0.1';
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
